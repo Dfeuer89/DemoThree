5c630bdc6b09432d89303bde4903d469352ceeee
dfeue <dfeue@WorkSmart.home>
2021-03-24 12:41:47 -0400

    Added Comment

--- src/demo4/GithubDemoFour.java
+++ src/demo4/GithubDemoFour.java
@@ -3,8 +3,11 @@ package demo4;
 public class GithubDemoFour {
 	public static void main (String [] args) {
 		
-		System.out.println("My First gitHub commit....");
+		System.out.println("My first gitHub commit....");
+		
+		System.out.println("I did it");
 		
 	}
 
+	//you added new code and now you have to share with the team
 }
